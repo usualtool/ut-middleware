5d5ea01d8c2ff80426c55bbc1156a3ba51aa6d71
黄豆 <292951110@qq.com>
2026-01-18 18:00:22 +0800

    Update SyncDispatcher.php

--- src/SyncDispatcher.php
+++ src/SyncDispatcher.php
@@ -1,6 +1,6 @@
 <?php
 namespace usualtool\Middleware;
-//同步模式
+//普通模式
 class SyncDispatcher{
     private array $stack = [];
     public function pipe($middleware): self{
@@ -14,16 +14,24 @@ class SyncDispatcher{
                 if (is_object($middleware) && method_exists($middleware, 'process')) {
                     return $middleware->process($req, new class($next) implements RequestHandlerInterface {
                         private $handler;
-                        public function __construct(callable $handler) { $this->handler = $handler; }
-                        public function handle($request) { return ($this->handler)($request); }
+                        public function __construct(callable $handler){
+                            $this->handler = $handler;
+                        }
+                        public function handle($request){
+                            return ($this->handler)($request);
+                        }
                     });
                 }
                 return $middleware($req, new class($next) implements RequestHandlerInterface {
                     private $handler;
-                    public function __construct(callable $handler) { $this->handler = $handler; }
-                    public function handle($request) { return ($this->handler)($request); }
+                    public function __construct(callable $handler){
+                        $this->handler = $handler;
+                    }
+                    public function handle($request){
+                        return ($this->handler)($request);
+                    }
                 });
-            }
+            };
         }
         return $next($request);
     }
