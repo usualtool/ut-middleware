fb97a79bab0445442ab23f7eb394df31e7be40ba
黄豆 <292951110@qq.com>
2026-01-18 16:20:03 +0800

    Create SwooleDispatcher.php

---
+++ src/SwooleDispatcher.php
@@ -0,0 +1,22 @@
+<?php
+namespace usualtool\Middleware;
+// Swoole 协程
+class SwooleDispatcher{
+    private array $stack = [];
+    public function pipe($middleware): self{
+        $this->stack[] = $middleware;
+        return $this;
+    }
+    public function dispatch($request, callable $finalHandler){
+        $next = $finalHandler;
+        foreach (array_reverse($this->stack) as $middleware) {
+            $next = function ($req) use ($middleware, $next) {
+                if (is_object($middleware) && method_exists($middleware, 'process')) {
+                    return $middleware->process($req, (object)['handle' => $next]);
+                }
+                return $middleware($req, (object)['handle' => $next]);
+            };
+        }
+        return $next($request);
+    }
+}
